c2d38f0e387eace88c01c5cc1863d05aa2f6777f
Azmiddin <tuitkf@9771yandex.ru>
2024-07-10 04:58:03 +0500

    progres

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
+#include "server/http_server.h"
 #include "utils/lrucache.h"
-#include <iostream>
-#include <utils/utils.h>
+#include "utils/utils.h"
 
+#include <boost/asio/io_context.hpp>
 #include <boost/program_options.hpp>
+#include <iostream>
 
 namespace po = boost::program_options;
 using server::utils::lrucache;
 
 int main(int argc, char** argv)
 {
-    server::utils::command_line_ptions options;
-    if(options(argc, argv))
-    {
-        std::clog <<"All to ok\n";
-        std::clog << options.root_directory <<"\n" << options.cache_size;
-    }
+    // server::utils::command_line_ptions options;
+    // if(options(argc, argv))
+    // {
+    //
+        server::HTTPServer server("/home/azmiddin",15, 4080);
+        server.run();
+
     return 0;
 }
