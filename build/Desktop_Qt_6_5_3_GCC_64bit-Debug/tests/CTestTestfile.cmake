88800e70a8075bd8abf94baf5aeb18b8da0a9f72
Azmiddin <tuitkf@9771yandex.ru>
2024-07-10 02:02:38 +0500

    del buil

--- build/Desktop_Qt_6_5_3_GCC_64bit-Debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/azmiddin/sampleServer/tests
-# Build directory: /home/azmiddin/sampleServer/build/Desktop_Qt_6_5_3_GCC_64bit-Debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LRUCacheTests "/home/azmiddin/sampleServer/build/Desktop_Qt_6_5_3_GCC_64bit-Debug/tests/runTests")
-set_tests_properties(LRUCacheTests PROPERTIES  _BACKTRACE_TRIPLES "/home/azmiddin/sampleServer/tests/CMakeLists.txt;7;add_test;/home/azmiddin/sampleServer/tests/CMakeLists.txt;0;")
